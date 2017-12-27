9d2605997008c28add7ab1ac612b7420714f5d52
jim <wasd2144@hotmail.com>
2017-12-27 11:08:30 +0800

    Add a new user and different permissions

--- src/common/menu.js
+++ src/common/menu.js
@@ -26,6 +26,7 @@ const menuData = [{
     path: 'step-form',
   }, {
     name: '高级表单',
+    role: 'admin',
     path: 'advanced-form',
   }],
 }, {
@@ -60,12 +61,14 @@ const menuData = [{
   name: '详情页',
   icon: 'profile',
   path: 'profile',
+  role: 'admin',
   children: [{
     name: '基础详情页',
     path: 'basic',
   }, {
     name: '高级详情页',
     path: 'advanced',
+    role: 'admin',
   }],
 }, {
   name: '结果页',
@@ -85,6 +88,7 @@ const menuData = [{
   children: [{
     name: '403',
     path: '403',
+    role: 'user',
   }, {
     name: '404',
     path: '404',
